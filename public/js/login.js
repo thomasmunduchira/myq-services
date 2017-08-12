9c2099138a651787f50b36b9dab6dc2ced4ce609
Thomas Munduchira <thomasjojimunduchira@gmail.com>
2017-08-11 23:15:01 -0700

    Use better message dialogs

--- public/js/login.js
+++ public/js/login.js
@@ -1,28 +1,55 @@
 function login() {
   var email = $("#email").val();
   var password = $("#password").val();
-  var message = $("#message");
-  message.html("");
 
   $.post("/login", {
     "email": email,
     "password": password
   }).done(function(response) {
-    message.html(response.message);
     if (response.redirectUri) {
-      window.location.href = response.redirectUri;
+      swal({
+        title: 'Success!',
+        text: 'You will be redirected back to Amazon',
+        type: 'success'
+      }).then(function() {
+        window.location.href = response.redirectUri;
+      }, function(dismiss) {
+
+      });
+    } else if (!response.success) {
+      swal({
+        title: 'Error',
+        text: response.message,
+        type: 'error',
+        timer: 6000
+      }).then(function() {
+
+      }, function(dismiss) {
+
+      });
+    } else {
+      swal({
+        title: 'Success!',
+        text: 'You were able to sign in to your MyQ account',
+        type: 'success',
+        timer: 2500
+      }).then(function() {
+
+      }, function(dismiss) {
+
+      });
     }
   }).fail(function(err) {
-    console.log(err);
+    console.error(err);
   });
 };
 
-$("#submit").on("click", function (event) {
+$("#submit").on("click", function(event) {
   event.preventDefault();
   login();
 });
 
-$("#login-form").keypress(function (event) {
+$("#login-form").keypress(function(event) {
   if (event.keyCode == 13) {
     event.preventDefault();
     login();
